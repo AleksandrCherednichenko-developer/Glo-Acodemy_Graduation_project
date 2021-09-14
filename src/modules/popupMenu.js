1658479eebdb5357eeabfa9c2aa50ea9db95d11e
AlexanderCherednicheenko <acherednichenko174@gmail.com>
2021-09-14 16:22:40 +0300

    изменение модуля popupMenu.js
    добавление функционала для блока "Не нашли услуги"

--- src/modules/popupMenu.js
+++ src/modules/popupMenu.js
@@ -4,8 +4,9 @@ const popupMenu = () => {
    const modalCallback = document.querySelector('.modal-callback');
    const modalOverlay = document.querySelector('.modal-overlay');
    const modalClose = document.querySelector('.modal-close img');
+   const questSectionBtn = document.querySelector('.quest-section .button');
 
-   const openToggle = () => {
+   const openPopup = () => {
       modalCallback.style.display = 'block';
       modalOverlay.style.display = 'block';
       // let start = Date.now();
@@ -15,22 +16,24 @@ const popupMenu = () => {
       //    if (timePassed > 500) clearInterval(timer);
       // }, 1.5);
    };
-   const closeToggle = () => {
+   const closePopup = () => {
       modalCallback.style.display = 'none';
       modalOverlay.style.display = 'none';
    };
 
    callbackBtn.forEach((elem) => {
       elem.addEventListener('click', () => {
-         openToggle();
+         openPopup();
       });
    });
 
    document.addEventListener('click', (event) => {
       if (event.target === modalClose){
-         closeToggle();
+         closePopup();
       } else if(event.target === modalOverlay){
-         closeToggle();
+         closePopup();
+      } else if(event.target === questSectionBtn){
+         openPopup();
       }
    });
 
